2707e7ac06dceda466f043e1da1a467ca660ef76
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-02-14 15:27:48 +0100

    Renamed get_location script to reflect better what it does, gets context and locations

--- others/get_context_&_locations.R
+++ others/get_context_&_locations.R
@@ -36,5 +36,28 @@ Amazon_speeches <- dplyr::filter(BR_Presidential_Speeches, grepl("amazon", text)
 Amazon_speeches$context <- poldis::context("amazon", var = Amazon_speeches$text, level = "sentences")
 Amazon_speeches$ccontext <- unlist(lapply(Amazon_speeches$context, function(x) paste(x, collapse = " | ")))
 
+# Get 2 lines before and 2 lines after
+context2 <- function(string, var) {
+  match <- paste0("([^.]+\\.){0,2}[^.]+(", string, ").*?\\.([^.]+\\.){0,2}")
+  s <- stringr::str_extract_all(var, match)
+  s
+}
+Amazon_speeches$AM2 <- context2("amazon", var = Amazon_speeches$text)
+Amazon_speeches$ID <- as.numeric(random::randomNumbers(n = 946, min = 1000, max = 2000, col = 1))
+
 # Save Rds
 saveRDS(Amazon_speeches, "amazon_speeches.Rds")
+
+# Get and save long data coding
+library(dplyr)
+amazon_speeches_long <- Amazon_speeches %>%
+  select(ID, president, year, party, title, location, date, text, AM2) %>% 
+  tidyr::unnest(cols = c(AM2))
+saveRDS(amazon_speeches_long, "amazon_speeches_long.Rds") # 2014 obs
+
+# Get a sample for hand coding
+training_set <- amazon_speeches_long %>%
+  select(ID, AM2) %>%
+  dplyr::slice_sample(n = 672)
+# export to Excel
+xlsx::write.xlsx(training_set, file = "training_set.xlsx")
