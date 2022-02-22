bc92ad41ffbc27e4d6a5e1f6c6600b849fb0a2ff
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-02-22 12:35:40 +0100

    Minor updates to merged training files and script

--- others/training_data_merge.R
+++ others/training_data_merge.R
@@ -9,19 +9,17 @@ l <- training_set_livio %>%
   rename(sov = sovereignty, ED = 'economic development', SD = 'social development',
          con = conservation, cm2 = 'comment 2') %>% 
   dplyr::mutate(comments = paste0("1 -", comment, " 2 - ", cm2)) %>% 
-  dplyr::select(ID, AM2, sov, ED, SD, con, comments) %>% 
-  arrange(ID)
+  dplyr::select(AM2, sov, ED, SD, con, comments)
+l <- tibble::rownames_to_column(l, "ID")
 
 h <- training_set_Sposito %>%
   rename(sov = Sovereignty, ED = 'Economic Development', SD = 'Social Development',
          con = Conservation, comments = Comments) %>% 
-  dplyr::select(ID, AM2, sov, ED, SD, con, comments) %>% 
-  arrange(ID)
+  dplyr::select(AM2, sov, ED, SD, con, comments)
+h <- tibble::rownames_to_column(h, "ID")
 
 # Get and merge only the comments data for later
-comments <- data.frame(ID = l$ID,
-                       comments = paste0(l$comments, " 3 - ", h$comments))
-comments <- arrange(comments, ID)
+comments <- data.frame(comments = paste0(l$comments, " 3 - ", h$comments))
 
 # Add the same comments for both datasets
 l <- select(l, -comments)
@@ -31,10 +29,9 @@ h$comments <- comments$comments
 
 # Merge and remove duplicates
 ts <- rbind(l, h)
-ts <- ts %>%
+ts <- ts %>% 
   dplyr::distinct() %>%
-  arrange(ID) %>%
-  mutate(dup = ifelse(duplicated(ID), 1, 0))
-
+  dplyr::mutate(dup = duplicated())
+  
 # Save data in excel format
 xlsx::write.xlsx(ts, file = "ts.xlsx")
