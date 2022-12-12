5c24a449a76ff3db2d210fda23983d2b5a7fac9e
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-12-12 09:24:50 -0300

    Updated data and replication script

--- Replication_Files/data_preparation.R
+++ Replication_Files/data_preparation.R
@@ -26,6 +26,10 @@ BR_Presidential_Speeches$text <- tm::stripWhitespace(BR_Presidential_Speeches$te
 BR_Presidential_Speeches$title <- tm::stripWhitespace(BR_Presidential_Speeches$title)
 BR_Presidential_Speeches$text <- textclean::add_comma_space(BR_Presidential_Speeches$text)
 BR_Presidential_Speeches$title <- textclean::add_comma_space(BR_Presidential_Speeches$title)
+BR_Presidential_Speeches$text <- stringr::str_squish(BR_Presidential_Speeches$text)
+BR_Presidential_Speeches$title <- stringr::str_squish(BR_Presidential_Speeches$title)
+BR_Presidential_Speeches$text <- trimws(BR_Presidential_Speeches$text)
+BR_Presidential_Speeches$title <- trimws(BR_Presidential_Speeches$title)
 
 # Make it latin SCII
 BR_Presidential_Speeches$text <- stringi::stri_trans_general(BR_Presidential_Speeches$text, id = "latin-ascii")
@@ -35,12 +39,29 @@ BR_Presidential_Speeches$title <- stringi::stri_trans_general(BR_Presidential_Sp
 Amazon_speeches <- dplyr::filter(BR_Presidential_Speeches, grepl("amazon", text))
 
 # Get 2 sentences before and 2 sentences after with poldis
-Amazon_speeches$AM2 <- poldis::extract_context("amazon", Amazon_speeches$text, "sentences", 2)
+Amazon_speeches$AM2 <- poldis::extract_context("amazon", Amazon_speeches$text,
+                                               "sentences", 2)
 
 # Get and save long data coding
 amazon_speeches_long <- Amazon_speeches %>%
   select(president, year, party, title, date, text, AM2) %>% 
   tidyr::unnest(cols = c(AM2))
+
+# More cleaning
+amazon_speeches_long$text <- stringr::str_squish(amazon_speeches_long$text)
+amazon_speeches_long$title <- stringr::str_squish(amazon_speeches_long$title)
+amazon_speeches_long$text <- trimws(amazon_speeches_long$text)
+amazon_speeches_long$title <- trimws(amazon_speeches_long$title)
+
+# Get locations from title
+amazon_speeches_long$location <- poldis::extract_location(amazon_speeches_long$title)
+#summary(as.factor(amazon_speeches_long$location))
+
+# Get location from text if not found
+amazon_speeches_long$location <- ifelse(grepl("^NA$", amazon_speeches_long$location),
+                                            poldis::extract_location(amazon_speeches_long$text),
+                                        amazon_speeches_long$location)
+#summary(as.factor(amazon_speeches_long$location))
 
 # save the data
 #saveRDS(amazon_speeches_long, "amazon_speeches_long.Rds") # 2048 obs
@@ -52,15 +73,11 @@ training_set_22 <- amazon_speeches_long %>%
 # export to Excel
 # xlsx::write.xlsx(training_set, file = "training_set.xlsx")
 
-# A few more things
-BR_Presidential_Speeches$text <- stringr::str_squish(BR_Presidential_Speeches$text)
-BR_Presidential_Speeches$title <- stringr::str_squish(BR_Presidential_Speeches$title)
+# Remove numbers and punctuation
 BR_Presidential_Speeches$text <- tm::removeNumbers(BR_Presidential_Speeches$text)
 BR_Presidential_Speeches$title <- tm::removeNumbers(BR_Presidential_Speeches$title)
 BR_Presidential_Speeches$text <- tm::removePunctuation(BR_Presidential_Speeches$text)
 BR_Presidential_Speeches$title <- tm::removePunctuation(BR_Presidential_Speeches$title)
-BR_Presidential_Speeches$text <- trimws(BR_Presidential_Speeches$text)
-BR_Presidential_Speeches$title <- trimws(BR_Presidential_Speeches$title)
 
 # Get locations from title
 BR_Presidential_Speeches$location <- poldis::extract_location(BR_Presidential_Speeches$title)
@@ -76,7 +93,7 @@ summary(as.factor(BR_Presidential_Speeches$location))
 # writexl::write_xlsx(BR_Presidential_Speeches, "BR_presid_speeches_final.xlsx")
 # Most of the NAs left are TV or Radio Announcements in case we want to mark these as such.
 BR_presid_speeches_final <- read_excel("data/BR_presid_speeches_final.xlsx")
-summary(as.factor(BR_presid_speeches_final$location))
+#summary(as.factor(BR_presid_speeches_final$location))
 
 # Last thing here, let's add a dummy variable for whether certain speech mentions the stem "amazon" or not.
 # As well as let's add a different categorical variable for location.
@@ -87,8 +104,8 @@ BR_presid_speeches_final <- BR_presid_speeches_final %>%
                                   grepl("Distrito Federal", location) ~ "Brasilia",
                                   grepl("NA", location) ~ "Non Identified",
                                   !grepl("Amazonian States|Non Amazonian States|Brasilia|Non Identified", location) ~ "International"))
-summary(as.factor(BR_presid_speeches_final$amazon_speech))
-summary(as.factor(BR_presid_speeches_final$location_cat))
+#summary(as.factor(BR_presid_speeches_final$amazon_speech))
+#summary(as.factor(BR_presid_speeches_final$location_cat))
 
 # save for analysis
-saveRDS(BR_presid_speeches_final, "BR_presid_speeches_final.Rds")
+#saveRDS(BR_presid_speeches_final, "BR_presid_speeches_final.Rds")
